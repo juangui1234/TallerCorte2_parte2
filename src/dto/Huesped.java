c18e3946e44dfb173fc6d390708f4069dc4b8fc7
Juan Guillermo Salazar <juan.guillermo.salazar@ibm.com>
2025-07-18 20:36:57 -0500

    Initial commit

---
+++ src/dto/Huesped.java
@@ -0,0 +1,53 @@
+package modelo;
+
+public class Huesped {
+
+    private String nombre;
+    private String documento;
+    private String correo;
+    private String telefono;
+
+    //constructor
+    public Huesped(String nombre, String documento, String correo, String telefono) {
+        this.nombre = nombre;
+        this.documento = documento;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    //getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    // Relación: 1 a muchos con dto.Reserva.
+
+}
